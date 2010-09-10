9d0380612a6dd59547321f11e77469e588d14147
MayDaniel <yamnad1@gmail.com>
2010-09-10 19:36:08 +0100

    A few updates.

--- src/genie/db.clj
+++ src/genie/db.clj
@@ -1,24 +1,37 @@
-(ns genie.db)
+(ns genie.db
+  (:require [somnium.congomongo :as db]))
 
-(mongo! :db "Genie")
+(db/mongo! :db "Genie")
 
 (defn fetch-user [username]
-  (dissoc :id (fetch-one :users :where {:username username})))
+  (dissoc :id (db/fetch-one :users :where {:username username})))
 
 (defn user-exists? [username]
   (boolean (fetch-user username)))
 
 (defn add-user! [{:keys [username password email]}]
-    (insert! :users {:username username
-                     :password password
-                     :email email
-                     :validated? false
-                     ;; :joined :updates :messages
-                     }))
+  (db/insert! :users {:username username
+                      :password password
+                      :email email
+                      :validated? false
+                      ;; :joined :updates :messages
+                      }))
 
 (defn update-user! [username f & args]
   (let [user (fetch-user username)]
-    (update! :users user (apply f user args))))
+    (db/update! :users user (apply f user args))))
 
 (defn destroy-user! [username]
-  (destroy! :users {:username username}))
+  (db/destroy! :users {:username username}))
+
+(defn add-validation! [username]
+  (let [id (rand-int 1e6)]
+    (update-user! username :validation-id id) id))
+
+(defn validated? [username]
+  (:validated? (fetch-user username)))
+
+(defn validate! [username]
+  (letfn [(update! [f & args] (apply update-user! username f args))]
+    (update! assoc :validated? true)
+    (update! dissoc :validation-id)))
--- src/genie/mail.clj
+++ src/genie/mail.clj
@@ -1,1 +1,23 @@
-(ns genie.mail)
+(ns genie.mail
+  (:require [clj-mail.core :as mail]
+            [clj-store.core :as store]
+            [clojure.set :as set]
+            [genie.db :as db]))
+
+(defn mail-credentials []
+  (set/rename-keys (store/in "mail.configuration")
+                   {:username :user
+                    :password pass
+                    :ssl? :ssl}))
+
+(defn send-message [email subject body]
+  (apply send-msg
+         :to email :subject subject :body body
+         (flatten (vec (mail-credentials)))))
+
+(defn send-validation [username email]
+  (send-message email
+                "Genie - Confirm your account"
+                (str "Validate your Genie account at: "
+                     "http://localhost:8080/" username "/"
+                     (db/add-validation! username))))
