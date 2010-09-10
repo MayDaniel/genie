f54566fe3fcbfbdb97656e6879c755e45edf0059
MayDaniel <yamnad1@gmail.com>
2010-09-11 00:33:00 +0100

    Typo.

--- src/genie/core.clj
+++ src/genie/core.clj
@@ -7,7 +7,7 @@
   (let [regexp {:user #"^[a-zA-Z0-9_]{3,12}$"
                 :email #"^[^@]{1,64}@[^@]{1,255}$"
                 :update #"^.{3,90}$"}]
-    (letfn [(re-check [[key coll]] (map #(if % (re-find (regexp key) % true)) coll))]
+    (letfn [(re-check [[key coll]] (map #(if % (re-find (regexp key) %) true) coll))]
       (->> {:user [username password] :email [email] :update [update]}
            (map re-check)
            (flatten)
@@ -16,7 +16,7 @@
 (defn register [{:keys [username password email] :as user}]
   (responses
    (cond (db/user-exists? username) :user-exists
-         (validate-all user) :invalid-characters
+         (not (validate-all user)) :invalid-characters
          :else (do (future (send-validation email))
                    (db/add-user! user)
                    :registration-success))))
