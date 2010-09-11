c81b0ca03312fdac2bb4240b02396e28a62a87e7
MayDaniel <yamnad1@gmail.com>
2010-09-11 19:43:41 +0100

    Added a few lines of documentation.

--- src/genie/date.clj
+++ src/genie/date.clj
@@ -13,12 +13,17 @@
                      "October" "November" "December"]))
 
 (defn date-to-string
+  "Returns a string of a date in the format \"11 September 2010 18:41:32\"
+   If no date is supplied, it defaults to now."
   ([] (date-to-string (now)))
   ([date] (let [date- (join " " ((juxt day #(months (month %)) year) date))
                 time- (join ":" ((juxt hour minute sec) date))]
             (str date- " " time-))))
 
-(defn string-to-date [date]
+(defn string-to-date
+  "Returns an org.joda.time.DateTime from a date that's been
+   represented as a string by \"date-to-string\"."
+  [date]
   (let [[day month year hour minute second] (split date #":| ")
         month (str ((map-invert months) month))]
     (apply date-time (map read-string [year month day hour minute second]))))
